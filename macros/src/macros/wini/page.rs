2e063610ae3414296698fc48622fbefa1f87738a
tk <tk>
2024-11-24 23:10:09 +0100

    0.1.1: Page arguments
    feat!: Allow the use of arguments from an axum route for pages

--- macros/src/macros/wini/page.rs
+++ macros/src/macros/wini/page.rs
@@ -13,15 +13,33 @@ pub fn page(args: TokenStream, item: TokenStream) -> TokenStream {
     parse_macro_input!(args with attr_parser);
 
 
-    // Modify the name of the current input to a reserved one
-    let mut input = parse_macro_input!(item as syn::ItemFn);
-    let original_name = input.sig.ident.clone();
+    // Modify the name of the original function to a reserved one
+    let mut original_function = parse_macro_input!(item as syn::ItemFn);
+    let original_name = original_function.sig.ident.clone();
     let new_name = Ident::new(
         &format!("__reserved_fn_wini_{}", original_name),
         original_name.span(),
     );
     // Change the function name
-    input.sig.ident = new_name.clone();
+    original_function.sig.ident = new_name.clone();
+
+    // Make so, that the new function has the same arguments as the previous one
+    let arguments = original_function.sig.inputs.clone();
+    let param_names = arguments
+        .iter()
+        .map(|arg| {
+            match arg {
+                syn::FnArg::Typed(pat_type) => {
+                    if let syn::Pat::Ident(param_name) = &*pat_type.pat {
+                        param_name.ident.clone()
+                    } else {
+                        panic!("Unsupported parameter pattern")
+                    }
+                },
+                syn::FnArg::Receiver(_) => panic!("self parameters not supported."),
+            }
+        })
+        .collect::<Vec<_>>();
 
 
     let (js_files, css_files) = get_js_and_css_files_in_dir();
@@ -34,11 +52,11 @@ pub fn page(args: TokenStream, item: TokenStream) -> TokenStream {
     // Generate the output code
     let expanded = quote! {
         #[allow(non_snake_case)]
-        #input
+        #original_function
 
         #[allow(non_snake_case)]
-        pub async fn #original_name() -> axum::response::Response<axum::body::Body> {
-            let html = #new_name().await;
+        pub async fn #original_name(#arguments) -> axum::response::Response<axum::body::Body> {
+            let html = #new_name(#(#param_names),*).await;
 
             let mut css_files: Vec<String> = (vec![#(#css_files)*] as Vec<&str>).into_iter().map(String::from).collect();
             let mut js_files: Vec<String> = (vec![#(#js_files)*] as Vec<&str>).into_iter().map(String::from).collect();
